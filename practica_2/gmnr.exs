be9e6580d15f0f4ee07ff474710284d68e57187e
carlogilmar <carlogilmar12@gmail.com>
2017-02-16 01:19:00 -0600

    try again to do my homework

--- practica_2/gmnr.exs
+++ practica_2/gmnr.exs
@@ -1,29 +1,37 @@
 defmodule Guess_my_number do
+
   def start_game(start_number..final_number,my_number) do
     guess(start_number..final_number,my_number)
   end
+
+
   defp guess(start_number..final_number,my_number) when not(my_number >= start_number and my_number <= final_number)  do
     IO.puts "Errozzz"
   end
+
+
   defp guess(start_number..final_number,my_number) do
     #Formula ((y-x)/2)+x
     pivot = div((final_number - start_number), 2) + start_number
     IO.puts "Pivot: #{pivot}, start_number: #{start_number}, final_number: #{final_number}, my_number: #{my_number}"
-    find_number(pivot,start_number..final_number,my_number)    
+    find_number(pivot,start_number..final_number,my_number)
   end
-  
-  defp find_number(pivot, _.._,my_number) when pivot == my_number do 
+
+
+  defp find_number(pivot, _.._,my_number) when pivot == my_number do
     IO.puts "Your number is #{my_number}"
   end
+
   defp find_number(pivot, start_number.._,my_number) when my_number < pivot do
     IO.puts "Calling my number is less than pivot"
     guess(start_number..(pivot),my_number)
   end
+
   defp find_number(pivot, _..final_number,my_number) when my_number > pivot do
     IO.puts "Calling my number is equals or greater than pivot"
     guess(pivot..final_number,my_number)
   end
 end
-#No olvidar el orden de las funciones 
+#No olvidar el orden de las funciones
 #El alcance de las funciones, las llamadas recursivas deben tener el mismo alcance, todas publicas o todas privadas
 #El compilador ayuda a depurar el codigo, por ejemplo variables no usadas
--- practica_2/guess.exs
+++ practica_2/guess.exs
@@ -1,18 +1,43 @@
-defmodule GuessMyNumber do
+defmodule Guess do
   IO.puts "Guess My Number :P---> inicio(1..100)"
 
   #Entra: numero
   #Sale: indicacion encontro o (se paso, le falta)
-  @goal 20
+  #@goal 20
 
   def inicio(tope) do
-    @algo = :rand.uniform(tope)
+     buscando(:rand.uniform(tope), tope)
   end
 
-  def algo(tope) when tope == @goal, do: IO.puts "es igual a a"
+  def buscando(n, tope) do
+    IO.puts "Buscando iterando"
+    comparador(propuesta(tope), n)
+  end
 
-  def buscar(n) when n>@goal, do: IO.puts "Es mayor a la meta"
-  def buscar(n) when n<@goal, do: IO.puts "Te falta ponle $5 mas"
+  def propuesta(tope) do
+    div(tope,2)
+  end
+
+  def comparador(number, goal) when number>goal do
+    IO.puts "#{number}Es mayor a la meta #{goal}"
+    tope=number*2
+    nTope = tope - 5
+    IO.puts "El tope es #{tope}, el nuevo tope sera #{nTope}"
+    buscando(goal, nTope)
+  end
+
+  def comparador(number, goal) when number<goal do
+    IO.puts "#{number} Te falta ponle $5 mas para llegar a #{goal}"
+    tope=number*2
+    nTope = tope + 5
+    IO.puts "El tope es #{tope}, el nuevo tope sera #{nTope}"
+    buscando(goal, nTope)
+
+  end
+
+  def comparador(number, goal) when number==goal do
+    IO.puts "Ok, el n umero es #{number} y la meta es #{goal}"
+  end
 
 
 end
