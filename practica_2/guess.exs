d99ec848ee0b043d0300eb7619db083afe3b4fb2
carlogilmar <carlogilmar12@gmail.com>
2017-02-17 16:13:45 -0600

    Ejercicio guess my number basico resuelto

--- practica_2/guess.exs
+++ practica_2/guess.exs
@@ -1,17 +1,16 @@
 defmodule Guess do
   IO.puts "Guess My Number :P---> inicio(1..100)"
 
-  #Entra: numero
-  #Sale: indicacion encontro o (se paso, le falta)
-  #@goal 20
-
   def inicio(tope) do
      buscando(:rand.uniform(tope), tope)
   end
 
   def buscando(n, tope) do
-    IO.puts "Buscando iterando"
     comparador(propuesta(tope), n)
+  end
+
+  def _buscando(n, tope)do
+    comparador(tope, n)
   end
 
   def propuesta(tope) do
@@ -19,25 +18,19 @@ defmodule Guess do
   end
 
   def comparador(number, goal) when number>goal do
-    IO.puts "#{number}Es mayor a la meta #{goal}"
-    tope=number*2
-    nTope = tope - 5
-    IO.puts "El tope es #{tope}, el nuevo tope sera #{nTope}"
-    buscando(goal, nTope)
+    IO.puts "Numero elegido: #{number} Es mayor a la meta #{goal}"
+    newNumber = number-1
+    _buscando(goal, newNumber)
   end
 
   def comparador(number, goal) when number<goal do
     IO.puts "#{number} Te falta ponle $5 mas para llegar a #{goal}"
-    tope=number*2
-    nTope = tope + 5
-    IO.puts "El tope es #{tope}, el nuevo tope sera #{nTope}"
-    buscando(goal, nTope)
-
+    newNumber = number+1
+    _buscando(goal, newNumber)
   end
 
   def comparador(number, goal) when number==goal do
-    IO.puts "Ok, el n umero es #{number} y la meta es #{goal}"
+    IO.puts "Ok, el numero es #{number} y la meta es #{goal}"
   end
-
 
 end
